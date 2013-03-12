c702a70d2b43ecfb990a19b9acdade7b4131e289
Adam Westerman <westeras@rose-hulman.edu>
2013-03-11 22:45:02 -0300

    edited parser call script
    attempting to get code running from the new GitHub repo

--- src/VcfParser/loadvcf.py
+++ src/VcfParser/loadvcf.py
@@ -1,5 +1,5 @@
 import vcf
-vcf_reader = vcf.Reader(open('/home/vcf_repository/vcf-analyzer/src/testvcf2.vcf'))
+vcf_reader = vcf.Reader(open('../testvcf2.vcf'))
 
 fileEnded = False
 while (not fileEnded):
