dd4596057d644876ce46434dc5804b1ab3bd92f2
Adam Westerman <westeras@rose-hulman.edu>
2013-03-13 15:40:19 -0300

    adding profiler functionality

--- src/VcfParser/loadvcf.py
+++ src/VcfParser/loadvcf.py
@@ -1,8 +1,13 @@
+import pycallgraph
+pycallgraph.start_trace()
+
 import vcf
 vcf_reader = vcf.Reader(open('../testvcf2.vcf'))
 
 fileEnded = False
 while (not fileEnded):
     fileEnded = vcf_reader.next()
+    
+pycallgraph.make_dot_graph('test.png')
 
 
