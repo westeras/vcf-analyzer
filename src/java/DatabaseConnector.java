71c11572014d06c69ada4e0f18b1a43318ece230
Julia Chapple <chappljd@rose-hulman.edu>
2013-03-13 14:44:21 -0400

    changes to connector

--- src/java/DatabaseConnector.java
+++ src/java/DatabaseConnector.java
@@ -4,39 +4,44 @@ import java.util.Properties;
 
 class DatabaseConnector
 {
-	private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-	private static final String DB_URL = "jdbc:mysql://localhost/vcf_analyzer";
-	
-	static final String USER = "vcf_user";
-	static final String PASS = "vcf";
-	
-	private Connection conn;
-	private	Statement stmt;
-	
-	public DatabaseConnector() throws SQLException
-	{
-		Connection conn = null;
-		Statement stmt = null;
+    private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    private static final String DB_URL = "jdbc:mysql://localhost/vcf_analyzer";
+    
+    static final String USER = "vcf_user";
+    static final String PASS = "vcf";
+    
+    private Connection conn;
+    private Statement stmt;
+    
+    public DatabaseConnector() throws SQLException
+    {
+        Connection conn = null;
+        Statement stmt = null;
 
-		Class.forName(JDBC_DRIVER);
+        Class.forName(JDBC_DRIVER);
 
-		conn = DriverManager.getConnection(DB_URL, USER, PASS);
-		stmt = conn.createStatement();
-		stmt.close();
-		conn.close();
-	
-	}
-	
-	public CloseConnection()
-	{
-		if (conn!=null) 
-		{
-			conn.close();
-		}
-		if (stmt!=null)
-		{
-			stmt.close();
-		}
-	}
+        conn = DriverManager.getConnection(DB_URL, USER, PASS);
+        stmt = conn.createStatement();
+        stmt.close();
+        conn.close();
+    
+    }
+    
+    public CloseConnection()
+    {
+        if (conn!=null) 
+        {
+            conn.close();
+        }
+        if (stmt!=null)
+        {
+            stmt.close();
+        }
+    }
+    
+    public String getVcfId()
+    {
+        return null
+    }
 
 }
