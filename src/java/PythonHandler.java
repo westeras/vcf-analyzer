2fc51fb5ec0ee4d9af0ffe180ffdb3f201a205b7
Adam Westerman <westeras@rose-hulman.edu>
2013-03-14 18:08:31 -0400

    added a new python handler class to invoke the python parser

---
+++ src/java/PythonHandler.java
@@ -0,0 +1,15 @@
+import org.python.core.*;
+import org.python.util.PythonInterpreter;
+
+class PythonHandler {
+	PythonInterpreter interpreter;
+	
+	public PythonHandler() {
+		interpreter = new PythonInterpreter();
+	}
+	
+	public invokeParser(String vcfName) throws FileNotFoundException {
+		return;
+	}
+	
+}
