5cf3ec270d971345bd1a028e97e5724914313c6e
Adam Westerman <westeras@rose-hulman.edu>
2013-03-14 18:38:09 -0400

    still working on making code run

--- src/java/PythonHandler.java
+++ src/java/PythonHandler.java
@@ -10,7 +10,7 @@ class PythonHandler {
 	
 	public void invokeParser(String vcfName) {
 		interpreter.exec("import sys");
-		interpreter.execfile("/home/git-vcf/src/VcfParser/loadvcf");
+		//interpreter.execfile("/home/git-vcf/src/VcfParser/loadvcf");
 	}
 	
 }
