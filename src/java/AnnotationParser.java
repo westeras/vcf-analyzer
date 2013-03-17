74fb01fb8cf9149af1bce9137191c77b51fb93a7
Daniel Schepers <schepedw@rose-hulman.edu>
2013-03-17 14:34:24 -0400

    Created Parser Classes

---
+++ src/java/AnnotationParser.java
@@ -0,0 +1,12 @@
+
+/**
+ * TODO Put here a description of what this class does.
+ *
+ * @author schepedw.
+ *         Created Mar 17, 2013.
+ */
+public class AnnotationParser {
+
+	private void parseLine(){}
+	private void parseFile(){}
+}
---
+++ src/java/DivergenceParser.java
@@ -0,0 +1,13 @@
+
+/**
+ * Consider creating a super class for this and AnnotationParser
+ *
+ * @author schepedw.
+ *         Created Mar 17, 2013.
+ */
+public class DivergenceParser {
+
+	
+	private void parseLine(){}
+	private void parseFile(){}
+}
