9d43ae0f99ea7ccd32f01cef19b4ddab7672e086
Adam Westerman <westeras@rose-hulman.edu>
2013-03-23 17:08:20 -0300

    filter creator partially implemented

--- src/java/FilterCreator.java
+++ src/java/FilterCreator.java
@@ -1,9 +1,32 @@
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 
 public class FilterCreator {
 	String[] commandList;
+	DatabaseConnector dbConnector;
+	int filterID;
+	private static ArrayList<String> operatorList = new ArrayList<String>(Arrays.asList("<", ">", "<=", ">=", "="));
 	
-	public FilterCreator(String[] commandList) {
+	public FilterCreator(String filterName, String[] commandList) throws ClassNotFoundException, SQLException {
 		this.commandList = commandList;
+		dbConnector = new DatabaseConnector();
+		
+		this.filterID = dbConnector.createFilter(filterName);
+		
+		for (int i = 0; i < this.commandList.length; i++) {
+			parseCommand(i);
+		}
 	}
 	
+	private void parseCommand(int index) {
+		String currentCommand = this.commandList[index];
+		for (int i = 0; i < operatorList.size(); i++) {
+			if (currentCommand.contains(operatorList.get(i))) {
+				String[] arguments = currentCommand.split(operatorList.get(i));
+				dbConnector.createFilterEntry(this.filterID, i, arguments);
+			}
+		}
+	}
 }
