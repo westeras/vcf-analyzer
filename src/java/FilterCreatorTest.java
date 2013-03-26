a3070a807516a48333b82926b59badf7c67c6ef6
Adam Westerman <westeras@rose-hulman.edu>
2013-03-25 22:08:25 -0400

    adding database check to filter creator test

--- src/java/FilterCreatorTest.java
+++ src/java/FilterCreatorTest.java
@@ -1,9 +1,40 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 
 public class FilterCreatorTest {
+	private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+	private static final String DB_URL = "jdbc:mysql://localhost/vcf_analyzer";
+	
+	static final String USER = "vcf_user";
+	static final String PASS = "vcf";
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		Connection conn = null;
+		Statement stmt = null;
+		
+		try {
+	
+			Class.forName(JDBC_DRIVER);
+	
+			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+			stmt = conn.createStatement();
+		}
+		catch (Exception e) {
+			throw new SQLException("Could not connect to database");
+		}
+		
+		String sql = "DELETE FROM `vcf_analyzer`.`Filter` WHERE `FilName`='testFilter'";
+		
+		try {
+			stmt.executeUpdate(sql);
+
+		} catch (SQLException se) {
+			throw new SQLException(se.getMessage());
+		}
+		
 		String filterName = "testFilter";
 		String[] operands = {"AC > 1", "AF = 0", "QD between 10 15"};
 		FilterCreator testCreator = new FilterCreator(filterName, operands);
