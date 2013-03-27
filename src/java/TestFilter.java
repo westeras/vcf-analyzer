123ed072f8b994cee268de04a7e73fe46013d0b2
Julia Chapple <chappljd@rose-hulman.edu>
2013-03-26 20:04:36 -0400

    test improved

--- src/java/TestFilter.java
+++ src/java/TestFilter.java
@@ -7,7 +7,7 @@
 public class TestFilter {
 
 	public static void main(String[] args) {
-		FilterApplier command = new FilterApplier("", "2013-03-25_09:51", "filteredData.txt" );
+		FilterApplier command = new FilterApplier("", "2013-03-25_09:51", "TestFile.txt" );
 		command.execute();
 	}
 
