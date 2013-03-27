18e5948e30458cb55a45a2449cfb834215fe1494
Adam Westerman <westeras@rose-hulman.edu>
2013-03-27 01:26:54 -0300

    Update testupload.py

--- src/VcfParser/testupload.py
+++ src/VcfParser/testupload.py
@@ -1,5 +1,5 @@
 import vcf
-vcf_reader = vcf.Reader(vcfName='testVCF_adam', fsock=open('../testVcf2.vcf'))
+vcf_reader = vcf.Reader(vcfName='testVCF_adam', fsock=open('../testvcf2.vcf'))
 
 fileEnded = False
 while (not fileEnded):
