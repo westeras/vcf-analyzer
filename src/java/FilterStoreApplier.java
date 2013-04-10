75896d0e5489dc72493b1b238e49fb63dea912f2
Julia Chapple <chappljd@rose-hulman.edu>
2013-04-09 22:17:23 -0400

    file name update

--- src/java/FilterStoreApplier.java
+++ src/java/FilterStoreApplier.java
@@ -1,15 +1,58 @@
+import java.sql.*;
 
 public class FilterStoreApplier extends FilterApplier
 {
+	
 
-    public FilterStoreApplier( String filterName, String vcfName )
-    {
-		this.filterName = filterName;
-		this.vcfName = vcfName;
-    }
+	public FilterStoreApplier(String vcfName, String filterName) {
+		super(vcfName, filterName);
+	}
 
-    protected String getSuccessMessage()
-    {
-    	return "Applied filter. Results are stored";
-    }    
+	protected String getSuccessMessage()
+	{
+		return String.format("Filtering complete. Results are stored");
+	}
+	
+	protected void initializeVcf( long vcfId ) throws Exception
+	{
+	
+	}
+	
+	protected void processUntestedEntry( ResultSet entries ) throws Exception
+	{
+
+	}
+	
+	protected void processUntestedEntryInfo( String tableName, ResultSet entryInfoData ) throws Exception
+	{
+
+	}
+	
+	protected void processPassingEntry( ResultSet entries ) throws Exception
+	{
+
+	}
+	
+	protected void processUntestedIndividual() throws Exception
+	{
+
+	}
+	
+	protected void processUntestedIndividualData( String genotypeName, ResultSet genotypeData ) throws Exception
+	{
+		
+	}
+	
+	protected void processPassingIndividual() throws Exception
+	{
+
+	}
+	
+	protected void finializeEntry() throws Exception
+	{
+	}
+	
+	protected void closeFiltering()
+	{
+	}
 }
--- src/java/FilterWriteApplier.java
+++ src/java/FilterWriteApplier.java
@@ -1,23 +1,79 @@
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-
-import javax.sql.*;
 
 public class FilterWriteApplier extends FilterApplier
 {
 
+	protected String fileName;
+	private VcfWriter writer;
+	
+	public FilterWriteApplier(String vcfName, String filterName, String fileName)
+	{
+		super(vcfName, filterName);
+		this.fileName = fileName;
+		this.writer = null;
+	}	
+	
+	public FilterWriteApplier(String vcfName, String filterName )
+	{
+		super(vcfName, filterName);
+		this.fileName = filterName + "-" + getDate();
+		this.writer = null;
+	}		
+	
+	protected String getSuccessMessage()
+	{
+		return String.format("Filtering complete see %s", this.fileName);
+	}
+	
+	protected void initializeVcf( long vcfId ) throws Exception
+	{
+		writer = new VcfWriter(this.fileName);
+		writer.writeHeader( this.connection.getVcfHeader(vcfId) );
+	}
+	
+	protected void processUntestedEntry( ResultSet entries ) throws Exception
+	{
+		writer.writeEntryStart( entries );
+	}
+	
+	protected void processUntestedEntryInfo( String tableName, ResultSet entryInfoData ) throws Exception
+	{
+		writer.writeInfoSection( tableName, entryInfoData );
+	}
+	
+	protected void processPassingEntry( ResultSet entries ) throws Exception
+	{
+		writer.writeEntryEnd(entries);
+	}
+	
+	protected void processUntestedIndividual() throws Exception
+	{
+		writer.writeIndividualStart();
+	}
+	
+	protected void processUntestedIndividualData( String genotypeName, ResultSet genotypeData ) throws Exception
+	{
+		writer.writeIndividualDatum( genotypeData, genotypeName);
+		
+	}
+	
+	protected void processPassingIndividual() throws Exception
+	{
+		writer.writeIndividualEnd();
+	}
+	
+	protected void finializeEntry() throws Exception
+	{
+		writer.writeEOL();
+	}
+	
+	protected void closeFiltering()
+	{
+		if (writer != null)
+		{
+			writer.closeWriter();
+		}
+	}
 
-    public FilterWriteApplier( String filterName, String vcfName, String filename )
-    {
-		this.filterName = filterName;
-		this.vcfName = vcfName;
-		this.fileName = filename;
-    }
 
-
-    protected String getSuccessMessage()
-    {
-    	return "Applied filter. See "+ this.fileName;
-    }
 }
