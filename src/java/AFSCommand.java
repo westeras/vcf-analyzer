36a558090699a37b49df713a958db36328ff6593
Daniel Schepers <schepedw@rose-hulman.edu>
2013-04-18 17:12:21 -0400

    Some more afs stuff

--- src/java/AFSCommand.java
+++ src/java/AFSCommand.java
@@ -1,11 +1,9 @@
-import java.lang.reflect.Array;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Collections;
-
-import java.util.List;
-import java.util.Set;
 import java.util.TreeMap;
 
 /**
@@ -14,65 +12,97 @@ import java.util.TreeMap;
  * @author schepedw. Created Apr 10, 2013.
  */
 public class AFSCommand extends Command {
-	private String options;
+	private String outputFileName;
 	private String VCFName;
 	private DatabaseConnector conn;
-	private TreeMap <Integer, Integer> spectra;
+	private TreeMap<Integer, Integer> spectra;
+	private String filterName;
 
-	public AFSCommand(String VCFName, String options)
+	public AFSCommand(String VCFName, String outputFilename, String filterName)
 			throws ClassNotFoundException, SQLException {
 		this.VCFName = VCFName;
-		this.options = options;
+		this.outputFileName = outputFilename;
+		this.filterName = filterName;
 		this.conn = new DatabaseConnector();
-		this.spectra=new TreeMap<Integer, Integer>();
+		this.spectra = new TreeMap<Integer, Integer>();
 	}
 
 	@Override
 	public String execute() {
-		
+
 		try {
 			ArrayList<String> vcfIDs = getVcfIDs();
-			
+
 			for (int i = 0; i < vcfIDs.size(); i++) {
 				ArrayList<String> entryIDs = getEntryIDs(vcfIDs.get(i));
 				for (int j = 0; j < entryIDs.size(); j++) {
-					int weirdThingsInEntry=0;
+					String filterEntryPassFlag=filterEntryPasses(entryIDs
+							.get(j));
+					
+					if (filterEntryPassFlag.equals("")){
+						System.out.println("Entry "+ entryIDs.get(j)+" not in filter. aborting");
+						return "error";
+					}
+					if (filterEntryPassFlag.equals("0")) {
+						continue;
+					}
+					
+					int weirdThingsInEntry = 0;
 					ArrayList<String> individualIDs = getIndividualIDs(entryIDs
 							.get(j));
 					for (int k = 0; k < individualIDs.size(); k++) {
 						ResultSet individuals = getIndividuals(individualIDs
-								.get(k));
-						
-						while (individuals.next()) {
-							weirdThingsInEntry+=countWeirdThings(individuals.getString("Allele1"),
-									individuals.getString("Allele2"),
-									individuals.getString("Allele3"));
-						
-						}
-						
+								.get(k),filterEntryPassFlag);
+						weirdThingsInEntry = countIndividuals(
+								weirdThingsInEntry, individuals);
 					}
-					
 					updateSpectra(weirdThingsInEntry);
 				}
 			}
 		} catch (Exception e) {
-			
+
 			e.printStackTrace();
 		}
 		printSpectra();
 		return this.spectra.toString();
 
 	}
+	
+	
+	
+
+	private int countIndividuals(int weirdThingsInEntry, ResultSet individuals)
+			throws SQLException {
+		while (individuals.next()) {
+			weirdThingsInEntry += countWeirdThings(
+					individuals.getString("Allele1"),
+					individuals.getString("Allele2"),
+					individuals.getString("Allele3"));
+
+		}
+		return weirdThingsInEntry;
+	}
+	
+	private String filterEntryPasses(String entryID)
+			throws ClassNotFoundException, SQLException {
+		if (this.filterName.equals("")) {
+			return "1";
+		}
+		String sql = "Select `FilterEntryPass`.`Pass` from `FilterEntryPass`,`Filter`  where `FilterEntryPass`.`EntryId`="+ entryID +
+				"`Filter`.`FilName`= '"+this.filterName+"' and `Filter`.`FilId` = `FilterEnteryPass`.`FilId";
+		ResultSet pass = this.conn.executeQuery(sql);
+		return pass.getString(1);
+
+	}
 
 	private void updateSpectra(int weirdThingsInEntry) {
-		if (!this.spectra.containsKey(weirdThingsInEntry)){
-			
-			this.spectra.put(weirdThingsInEntry,1);
-		}
-		else {
-			int update= this.spectra.get(weirdThingsInEntry)+1;
-			
-			this.spectra.put(weirdThingsInEntry,update);
+		if (!this.spectra.containsKey(weirdThingsInEntry)) {
+
+			this.spectra.put(weirdThingsInEntry, 1);
+		} else {
+			int update = this.spectra.get(weirdThingsInEntry) + 1;
+
+			this.spectra.put(weirdThingsInEntry, update);
 		}
 	}
 
@@ -101,9 +131,15 @@ public class AFSCommand extends Command {
 		return convertToArrayList(individualIds);
 	}
 
-	private ResultSet getIndividuals(String indId)
+	private ResultSet getIndividuals(String indId, String checkIndividualFlag)
 			throws ClassNotFoundException, SQLException {
-		String sql = "Select * from `GT` where `IndID`= '" + indId + "'";
+		String sql;
+		if (checkIndividualFlag.equals("2")) {
+			sql="Select * from `GT`, `FilterIndividualPass` where `GT`.`IndID`= '" + indId + "' and `FilterIndividualPass`.`IndID`='"+indId+"' and `FilterIndividualPass`.`IndID`=1";
+		} else {
+			sql = "Select * from `GT` where `IndID`= '" + indId + "'";
+		}
+
 		return this.conn.executeQuery(sql);
 	}
 
@@ -118,54 +154,65 @@ public class AFSCommand extends Command {
 	}
 
 	private int countWeirdThings(String allele1, String allele2, String allele3) {
-		int count=0;
-		
+		int count = 0;
+
 		if (allele1 == null) {
 			return count;
 		} else if (allele1.equals("1")) {
-			
+
 			count++;
 		}
 		if (allele2 == null) {
 			return count;
-		}
-		else if (allele2.equals("1")) {
-			
+		} else if (allele2.equals("1")) {
+
 			count++;
 		}
 		if (allele3 == null) {
 			return count;
-		}
-		else if (allele3.equals("1")) {
-			
+		} else if (allele3.equals("1")) {
 			count++;
 		}
 		return count;
 	}
 
 	private void printSpectra() {
-		int endOfSpectra=this.spectra.lastKey();
-		
-		for (int i=0; i <=endOfSpectra; i ++){
-			if (!this.spectra.containsKey(i)){
-				System.out.printf("%d\t",0);
+		String output = getOutputContent();
+		if (this.outputFileName.equals("")) {
+			System.out.println(output);
+		} else {
+			FileWriter fstream;
+			try {
+				fstream = new FileWriter(this.outputFileName);
+				BufferedWriter out = new BufferedWriter(fstream);
+				out.write(output);
+				out.close();
+			} catch (IOException exception) {
+				exception.printStackTrace();
 			}
-			else 
-				System.out.printf("%d\t",this.spectra.get(i));
 		}
-		System.out.printf("\n");
-		for (int i =0;i<=endOfSpectra;i++){
-			System.out.printf("%d\t", i );
-		}
-		System.out.printf("\n");
 	}
-	
-	
+
+	private String getOutputContent() {
+		String output = "";
+		int endOfSpectra = this.spectra.lastKey();
+		for (int i = 0; i <= endOfSpectra; i++) {
+			if (!this.spectra.containsKey(i)) {
+				output += String.format("%d\t", 0);
+			} else
+				output += String.format("%d\t", this.spectra.get(i));
+		}
+		output += String.format("\n");
+		for (int i = 0; i <= endOfSpectra; i++) {
+			output += String.format("%d\t", i);
+		}
+		output += String.format("\n");
+		return output;
+	}
 
 	@Override
 	public void pipeOutput() {
 		// TODO Auto-generated method stub.
 	}
-
 
 }
