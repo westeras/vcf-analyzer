9ae8ecc81ffd67563a259f85ee8f3091fe007538
David Galvez <galvezdm@rose-hulman.edu>
2013-04-26 09:42:23 -0400

    Further command revamping

--- src/java/CommandLineInterpreter.java
+++ src/java/CommandLineInterpreter.java
@@ -81,6 +81,80 @@ public class CommandLineInterpreter
 			}
 			result = vcfCommand(commandLineArguments);
 		}
+		
+		if (commandLineArguments[0].equals("delete")){						
+			Command makeView = new DeleteCommand(commandLineArguments[1],commandLineArguments[2],commandLineArguments[3]);
+			return makeView.execute();
+		}
+		
+		if(commandLineArguments[0].equals("create")
+				&& commandLineArguments[1].equals("filter")){
+			FilterCreator filter = null;
+			if(commandLineArguments.length == 2){
+				input = new Scanner(System.in);
+				ArrayList<String> additionalArguments = new ArrayList<String>();
+				System.out.println("Please input additional arguments for creating a filter. Enter 'done' or hit enter twice when finished.");
+				while(true){
+					System.out.print(">> ");
+					String line = input.nextLine().trim();
+					if(line.equals("done") || line.equals("")){
+						break;
+					}
+					System.out.println(line);
+					additionalArguments.add(line);
+				}
+				String[] arguments = new String[additionalArguments.size()];
+				arguments = additionalArguments.toArray(arguments);
+				filter = new FilterCreator(commandLineArguments[0],arguments);
+			}else{
+				String[] additionalArguments = new String[commandLineArguments.length-2];
+				
+				for(int i = 0; i < additionalArguments.length; i++){
+					additionalArguments[i] = commandLineArguments[i+2];
+				}
+				
+				filter = new FilterCreator(commandLineArguments[0],additionalArguments);
+			}
+			filter.uploadEntries();
+		}
+		
+		if(commandLineArguments[0].equals("crefil")){
+			FilterCreator filter = null;
+			if(commandLineArguments.length == 1){
+				input = new Scanner(System.in);
+				ArrayList<String> additionalArguments = new ArrayList<String>();
+				System.out.println("Please input additional arguments for creating a filter. Enter 'done' or hit enter twice when finished.");
+				while(true){
+					System.out.print(">> ");
+					String line = input.nextLine().trim();
+					if(line.equals("done") || line.equals("")){
+						break;
+					}
+					System.out.println(line);
+					additionalArguments.add(line);
+				}
+				String[] arguments = new String[additionalArguments.size()];
+				arguments = additionalArguments.toArray(arguments);
+				filter = new FilterCreator(commandLineArguments[0],arguments);
+			}else{
+				String[] additionalArguments = new String[commandLineArguments.length-1];
+				
+				for(int i = 0; i < additionalArguments.length; i++){
+					additionalArguments[i] = commandLineArguments[i+1];
+				}
+				
+				filter = new FilterCreator(commandLineArguments[0],additionalArguments);
+			}
+			filter.uploadEntries();
+		}
+		
+		if(commandLineArguments[0].equals("filter")){
+			String[] args = new String[commandLineArguments.length - 1];
+			for(int i = 0; i < args.length; i++){
+				args[i] = commandLineArguments[i+1];
+			}
+			result = filterCommand(commandLineArguments);
+		}
 			/*if (commandLine.hasOption("upano")){
 				result = uploadCommand(commandLineArguments, commandLine, "upano");
 			}
@@ -183,6 +257,32 @@ public class CommandLineInterpreter
 		return result;
 	}  
 	
+	private static String filterCommand(String[] args) {
+		FilterApplier applier  = null;
+		
+		String result = "";
+		String store = "";
+		String write = "";
+		String by = "";
+		String vcf = "";
+		
+		for(int i = 0; i < args.length; i++){
+			if(args[i].equals("store") && i != args.length - 1){store = args[i+1];}
+			if(args[i].equals("write") && i != args.length - 1){write = args[i+1];}
+			if(args[i].equals("by") && i != args.length - 1){by = args[i+1];}
+			if(args[i].equals("vcf") && i != args.length - 1){vcf = args[i+1];}
+		}
+		
+		if(vcf.equals("")){return "Please include the vcf that is being used.";}
+		if(store.equals("") && write.equals("")){return "Please include the file name output.";}
+		if(by.equals("")){return "Please include what this is being filtered by";}
+		
+		if(!write.equals("")){applier = new FilterWriteApplier(vcf, by, write);}
+		if(!store.equals("")){applier = new FilterWriteApplier(vcf, by, store);}
+		
+		return result;
+	}
+
 	private static String vcfCommand(String[] args) throws ClassNotFoundException, SQLException {
 		Command command = null;
 		
@@ -192,9 +292,9 @@ public class CommandLineInterpreter
 		String filterName = "";
 		
 		for(int i = 0; i < args.length; i++){
-			if(args[i].equals("-name") && i != args.length - 1){fileName = args[i+1];}
-			if(args[i].equals("-file") && i != args.length - 1){fileLocation = args[i+1];}
-			if(args[i].equals("-filter") && i != args.length - 1){filterName = args[i+1];}
+			if(args[i].equals("name") && i != args.length - 1){fileName = args[i+1];}
+			if(args[i].equals("file") && i != args.length - 1){fileLocation = args[i+1];}
+			if(args[i].equals("filter") && i != args.length - 1){filterName = args[i+1];}
 		}
 		
 		if(fileLocation.equals("") && fileName.equals("") && filterName.equals("")){return "Please input proper arguments";}
@@ -222,8 +322,8 @@ public class CommandLineInterpreter
 		String fileName = "";
 		
 		for(int i = 0; i < args.length; i++){
-			if(args[i].equals("-name") && i != args.length - 1){fileName = args[i+1];}
-			if(args[i].equals("-file") && i != args.length - 1){fileLocation = args[i+1];}
+			if(args[i].equals("name") && i != args.length - 1){fileName = args[i+1];}
+			if(args[i].equals("file") && i != args.length - 1){fileLocation = args[i+1];}
 		}
 		
 		if(fileLocation.equals("") && fileName.equals("")){return "Please input proper arguments";}
